a1bb41b18d82c8117b7dcfc249b0166f40bc617d
Dvdhan <han2041126@gmail.com>
2023-05-09 17:37:37 +0900

    feat: 초기 생성 및 연동

---
+++ src/test/java/Solo_Project/Library_API/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Solo_Project.Library_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
