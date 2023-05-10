6d7d72de7d8f648d0659fe34dbe34b4ae5e07d63
Dvdhan <han2041126@gmail.com>
2023-05-10 20:22:31 +0900

    feat: memberBook 엔티티에 createdAt, returnedAt 추가

--- src/main/java/Solo_Project/Library_API/domain/libraryMember/entity/LibraryMember.java
+++ src/main/java/Solo_Project/Library_API/domain/libraryMember/entity/LibraryMember.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
--- src/main/java/Solo_Project/Library_API/domain/memberBook/entity/MemberBook.java
+++ src/main/java/Solo_Project/Library_API/domain/memberBook/entity/MemberBook.java
@@ -4,12 +4,22 @@ import Solo_Project.Library_API.domain.book.entity.Book;
 import Solo_Project.Library_API.domain.member.entity.Member;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class MemberBook {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long memberBookId;
+
+    @Column
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime returnedAt;
+
+    @Column
+    private Long libraryId;
 
     @ManyToOne
     @JoinColumn(name = "memberId")
